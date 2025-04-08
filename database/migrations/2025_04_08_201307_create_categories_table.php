96d396b6a82843a6576018f2f2c8e6af3f0eae0c
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-08 21:46:35 +0100

    update migration catégories

--- database/migrations/2025_04_08_201307_create_categories_table.php
+++ database/migrations/2025_04_08_201307_create_categories_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
