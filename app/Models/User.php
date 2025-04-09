10da2b1c1fcd9b8523963d2e6869c9d78a8cb672
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-09 11:44:44 +0100

    add les methode de check in user (admin,client or partenaire)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+   protected $fillable =[
+    'name',
+    'prenom',
+    'email',
+    'password',
+    'telephone',
+    'addresse',
+    'ville',
+    'code_postal',
+    'pays',
+    'role',
+   ];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+   protected $hidden=[
+    'password',
+    'remember_token',
+   ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+   protected $casts =[
+    'email_verified_at' =>'datetime',
+    'password' =>'hashed',
+   ];
+//check if user is admin
+   public function isAdmin(){
+    return $this->role === 'admin';
+   }
 }
