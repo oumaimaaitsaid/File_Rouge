5ed558d6d3c5d732534f9b04d4f4b70eb92c65bc
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-09 22:54:10 +0100

    ajouter en controller product les conditions pour order les produits

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -19,7 +19,42 @@ class ProductController extends Controller
                 $q->where('slug', $request->category);
             });
         }
-        
+
+        //order avec les produits
+
+        if($request->has('sort')){
+            switch($request->sort){
+                case 'price_asc':
+                    $query->orderByRaw('COALESCE(prix_promo,prix) ASC');
+                    break;
+                case 'price_desc':
+                    $query->orderByRaw('COALESCE(prix_promo,prix) DESC');
+                    break;
+                case 'name_asc':
+                    $query->orderBy('nom','asc');   
+                    break;
+                case 'name_desc':
+                    $query->orderBy('nom','desc');
+                    break;
+                case 'newest':
+                    $query->orderBy('created_at','desc');
+                    break;
+                case 'oldest':
+                    $query->orderBy('created_at','asc');
+                    break;
+                case 'popular':
+                    $query->withCount(['avis']=> function($q){
+                        $q->where('approuve',true);
+                    })
+                    ->orderByDesc('avis_count','desc');
+                    break;
+                    default:
+                    $query->orderBy('created_at','desc');
+            }
+            else{
+                $query->orderBy('created_at','desc');
+            }
+        }
 
     }
 }
