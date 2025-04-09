2d2344dd1e632ceaabd8ec5a1d11a2bf2b28c5c1
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-09 22:42:58 +0100

    ajouter function index in controller product avec condition de filtrage by category

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Produit;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request){
+        $query= Produit::where('disponible',true)
+        ->with(['categorie','imagePrincipale']);
+   
+        //filtrage by category
+
+        if($request->has('category') && !empty($request->category)){
+            $query->whereHas('categorie',function($q) use ($request){
+                $q->where('slug', $request->category);
+            });
+        }
+        
+
+    }
 }
