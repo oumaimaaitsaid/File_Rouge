5aaaa161381a57beba18b66eb9a3b7bfbf664701
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-09 16:03:06 +0100

    enregister les middlewares dans bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'partenaire' => \App\Http\Middleware\PartenaireMiddleware::class,
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
