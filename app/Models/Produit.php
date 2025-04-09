342a26909a5268f567129279679403a4c327b5e7
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-09 22:12:30 +0100

    ajouter les fillables et les cast de model produit

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produit extends Model
 {
-    //
+   use HasFactory;
+   protected $fillable =[
+    'nom',
+    'slug',
+    'description',
+    'ingredients',
+    'prix',
+    'prix_promo',
+    'stock',
+    'disponible',
+    'featured',
+    'category_id',
+   ];
+   protected $casts =[
+   'prix'=>'float',
+    'prix_promo' =>'float',
+    'stock'=>'integer',
+    'disponible'=>'boolean',
+    'featured'=>'boolean',
+   ];
 }
