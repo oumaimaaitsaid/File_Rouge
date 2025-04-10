8111e177b9356312987059ee096c0cc66240180d
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-10 22:20:12 +0100

    Products: complète l' Implémentation des contrôleurs API pour la gestion des produits (client et admin)

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable =[
+        'nom',
+        'slug',
+        'description',
+        'image',
+        'active',
+    ];
+    protected $casts =[
+        'active'=>'boolean',
+    ];
+    public function produits(){
+        return $this->hasMany(Produit::class ,'category_id');
+    }
 }
