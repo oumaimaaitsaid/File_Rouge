e30fbba771258015c3293870c08740e71cc69b99
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-10 22:10:22 +0100

    ajoute es fillable et les cast dans model ImageProduit

--- app/Models/ImageProduit.php
+++ app/Models/ImageProduit.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ImageProduit extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable =[
+        'produit_id',
+        'chemin',
+        'principale',
+        'order'
+    ];
+    protected $casts =[
+        'principale'=>'boolean',
+        'order'=>'integer'
+    ];
+    
+
 }
