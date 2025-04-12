0ef4ad9ded66998974b4195644dcf939185f7e17
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-12 21:05:53 +0100

    Ajout du middleware CartSessionMiddleware pour gérer l'attribution d'un cookie de session pour le panier

---
+++ app/Http/Middleware/CartSessionMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+
+class CartSessionMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        
+        if ($request->attributes->has('set_cart_cookie')) {
+            $sessionId = $request->attributes->get('set_cart_cookie');
+            $response->cookie('cart_session_id', $sessionId, 60 * 24 * 30); // 30 jours
+        }
+        
+        return $response;
+    }
+}
