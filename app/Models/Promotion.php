351646e36ba6dc3cecc49dc5bebae1e7ff35a1c9
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 22:23:48 +0100

    Définir les propriétés fillables pour le modèle Promotion

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Promotion extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'description',
+        'type',
+        'valeur',
+        'montant_minimum',
+        'utilisation_max',
+        'utilisation_actuelle',
+        'usage_unique_par_client',
+        'date_debut',
+        'date_fin',
+        'active'
+    ];
+
+   
+
+   
+  
+
+   
+   
+   
+   
+
 }
