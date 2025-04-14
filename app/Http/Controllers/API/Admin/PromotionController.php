7acdccd725fa573f1522dc1f0ae29b00aa178bf7
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 22:35:35 +0100

    Créer PromotionController (Admin) pour gérer les promotions via l'API

--- app/Http/Controllers/API/Admin/PromotionController.php
+++ app/Http/Controllers/API/Admin/PromotionController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\API\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Promotion;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
 
 class PromotionController extends Controller
 {
-    //
+   
+   
+
+    
+    
+
+    
+
+    
+
+   
+   
+    
 }
