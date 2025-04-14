0e52db39e43520980da82ed445f0c283434b9e27
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-04-14 22:29:47 +0100

    Spécifier la table personnalisée 'promotion_utilisations' dans le modèle PromotionUtilisation

---
+++ app/Models/PromotionUtilisation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PromotionUtilisation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'promotion_utilisations';
+
+   
+
+  
+   
+    
+
+   
+}
