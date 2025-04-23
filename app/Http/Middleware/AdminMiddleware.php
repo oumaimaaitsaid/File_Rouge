7a6661d9db7e6debb6b5668f7500ca70b3c0fa00
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-04-23 21:02:34 +0100

    ajouter middleware d admin

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::check() && Auth::user()->isAdmin())
+        return $next($request);
+    }
+    return redirect()->route('home')->with('error', 'Vous n\'avez pas les autorisations nécessaires pour accéder à cette page.');
+}
