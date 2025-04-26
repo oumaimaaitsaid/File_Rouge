30436f3674812eadef0445a0495d9c3e342f5c10
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-04-26 20:42:52 +0100

    ajouter le lien de use aPP\mODELS _COMMANDES

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Commande;
+use Illuminate\Support\Facades\Auth;
 class OrderController extends Controller
 {
     public function index()
