5e8f85a988e2bdebf0897c85b2f56257c8a2d17f
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-04-28 22:44:50 +0100

    creation service FideliteSirvice

---
+++ app/Services/FideliteService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\Commande;
+use App\Models\Promotion;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+
+class FideliteService
+{
+   
+    
+  
+  
+  
+}
